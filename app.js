7551afbc654c0fdacabe9c1c5b14cb971cc6d56f
Mariam <74742426+mariamdateshidze@users.noreply.github.com>
2022-04-07 15:03:58 +0400

    add html and js files

---
+++ app.js
@@ -0,0 +1,44 @@
+
+// 1
+let myNums = [4, 3, 9, 7, 1];
+
+console.log((myNums[0]+ myNums[1]+ myNums[2]+myNums[3]+myNums[4]) / myNums.length)
+
+// 2
+let myArr =[
+    {
+        name: "mariam",
+		age: 19,
+    },
+    {
+        name: "ana",
+		age: 23,
+    },
+    {
+        name: "lika",
+		age: 32,
+    },
+    {
+        name: "qeti",
+		age: 35,
+    },
+    {
+        name: "nini",
+		age: 12,
+    },
+];
+
+console.log(myArr);
+
+// 3
+
+let person = {
+	firstName: "mariam",
+	lastName: "dateshidze",
+	address: ["addressfirst", "addresstwo"],
+	age: 19,
+	phoneNumbers:[555443322, 555323232],
+};
+
+// 4
+console.log(`My name is ${person.firstName}, My age is ${person.age}, My address is ${person.address[0]}`);
